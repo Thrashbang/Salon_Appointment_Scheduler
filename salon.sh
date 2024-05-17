7c5b93ad4f2a359054e5976cc6e5046bf2e9833b
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2024-05-16 21:13:14 -0600

    salon.sh

--- salon.sh
+++ salon.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-psql --username=freecodecamp --dbname=salon -c "TRUNCATE customers, appointments, services;"
 psql --username=freecodecamp --dbname=salon -c "CREATE TABLE customers(customer_id SERIAL PRIMARY KEY);"
 psql --username=freecodecamp --dbname=salon -c "CREATE TABLE appointments(appointment_id SERIAL PRIMARY KEY);"
 psql --username=freecodecamp --dbname=salon -c "CREATE TABLE services(service_id SERIAL PRIMARY KEY);"
@@ -8,12 +7,12 @@ psql --username=freecodecamp --dbname=salon -c "ALTER TABLE appointments ADD COL
 psql --username=freecodecamp --dbname=salon -c "ALTER TABLE appointments ADD COLUMN service_id INT REFERENCES services(service_id);"
 psql --username=freecodecamp --dbname=salon -c "ALTER TABLE customers ADD COLUMN phone VARCHAR(20) UNIQUE;"
 psql --username=freecodecamp --dbname=salon -c "ALTER TABLE customers ADD COLUMN name VARCHAR(25);"
-psql --username=freecodecamp --dbname=salon -c "ALTER TABLE services ADD COLUMN name VARCHAR(25);"
+psql --username=freecodecamp --dbname=salon -c "ALTER TABLE services ADD COLUMN name VARCHAR(25) UNIQUE;"
 psql --username=freecodecamp --dbname=salon -c "ALTER TABLE appointments ADD COLUMN time VARCHAR(5);"
-psql --username=freecodecamp --dbname=salon -c "INSERT INTO services(name) VALUES('cut'), ('color'), ('style');"
+psql --username=freecodecamp --dbname=salon -c "INSERT INTO services(name) VALUES('cut'), ('color'), ('perm'), ('style'), ('trim');"
 
 
-echo -e "Welcome to My Salon. What service would you like?\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
 
 readService() {
 
@@ -30,7 +29,13 @@ readService() {
     elif [[ $service == "color" ]]
     then
       echo -e "$id) "$service""
+    elif [[ $service == "perm" ]]
+    then
+      echo -e "$id) "$service""
     elif [[ $service == "style" ]]
+    then
+      echo -e "$id) "$service""
+      elif [[ $service == "trim" ]]
     then
       echo -e "$id) "$service""
     fi
@@ -38,28 +43,48 @@ readService() {
 
   read SERVICE_ID_SELECTED
 
-  if [[ $SERVICE_ID_SELECTED -gt 3 ]]
+  if [[ $SERVICE_ID_SELECTED -gt 5 ]]
   then
-    echo -e "Service not found. What service would you like?\n"
+    echo -e "\nI could not find that service. What would you like today?"
     readService
   elif [[ $SERVICE_ID_SELECTED -lt 1 ]]
   then
-    echo -e "Service not found. What service would you like?\n"
+    echo -e "\nI could not find that service. What would you like today?"
     readService
   else
-    echo "What's your phone number?"
+    echo -e "\nWhat's your phone number?"
     read CUSTOMER_PHONE
-    phoneQuery="$($PSQL "SELECT name FROM customers WHERE phone='$CUSOMER_PHONE'")"
-    echo $phoneQuery | while IFS=" " read title name rows paren
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE';")
+
+    if [[ -z$phoneQuery ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      ADD_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME');")
+    fi
+
+    SERVICE=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED;")
+
+    echo $SERVICE | while read title hyphens name num close
     do
-      if [[ $name == "------" ]]
-      then
-        echo "Phone number not recognized. What is your name?"
-      fi
+      echo -e "\nWhat time would you like your $name, $CUSTOMER_NAME?"
+    done
+    read APPOINTMENT_TIME
+
+    echo $SERVICE | while read title hyphens name num close
+    do
+      echo -e "\nI have put you down for a $name at $APPOINTMENT_TIME, $CUSTOMER_NAME."
+    done
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    echo $CUSTOMER_ID | while read title hyphens id num close
+    do
+      SCHEDULE_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($id, $SERVICE_ID_SELECTED, '$APPOINTMENT_TIME')")
     done
   fi
  
 }
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
 readService
 
