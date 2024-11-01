7999acb02a71486cd4d3742d69273f9a8f7d1d87
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2024-11-01 13:36:34 -0600

    Update salonNew.sh

--- salonNew.sh
+++ salonNew.sh
@@ -1,17 +1,31 @@
 !#/bin/bash
 
-echo $(PSQL --username=freecodecamp --dbname=salon -c "SELECT * FROM services") | while read id pipe service
-do
-  if [[ $service == "Haircut" ]]
-  then
-    echo -e "$id) "$service""
-  elif [[ $service == "Hair Coloring" ]]
-  then
-    echo -e "$id) "$service""
-  elif [[ $service == "Trim" ]]
-  then
-    echo -e "$id) "$service""
-  fi
-done
+readService() {
 
-read $
+  echo $(PSQL --username=freecodecamp --dbname=salon -c "SELECT * FROM services") | while read id pipe service
+  do
+    if [[ $service == "Haircut" ]]
+    then
+      echo -e "$id) "$service""
+    elif [[ $service == "Hair Coloring" ]]
+    then
+      echo -e "$id) "$service""
+    elif [[ $service == "Trim" ]]
+    then
+      echo -e "$id) "$service""
+    fi
+  done
+
+  read SERVICE_ID
+
+  if [[ $SERVICE_ID -gt 3 ]]
+  then
+    echo -e "\nI could not find that service. What would you like today?"
+    readService
+  elif [[ $SERVICE_ID -lt 1 ]]
+  then
+    echo -e "\nI could not find that service. What would you like today?"
+    readService
+  fi
+
+}
