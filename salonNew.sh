3c440cf5c1a63791d14fc8adbda659a7ad95a505
Thrashbang <159561923+Thrashbang@users.noreply.github.com>
2024-11-01 12:02:09 -0600

    Update salonNew.sh

--- salonNew.sh
+++ salonNew.sh
@@ -1,5 +1,21 @@
 !#/bin/bash
 
-read $SERVICE
-
-if [[ -1 ]]
+  echo "$services_list" | while read id pipe service
+  do
+    if [[ $service == "cut" ]]
+    then
+      echo -e "$id) "$service""
+    elif [[ $service == "color" ]]
+    then
+      echo -e "$id) "$service""
+    elif [[ $service == "perm" ]]
+    then
+      echo -e "$id) "$service""
+    elif [[ $service == "style" ]]
+    then
+      echo -e "$id) "$service""
+      elif [[ $service == "trim" ]]
+    then
+      echo -e "$id) "$service""
+    fi
+  done
